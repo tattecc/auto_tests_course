591ffe1cb65aaec8300b23d78940f43452e6ec6b
tattecc <tatt.ecc@gmail.com>
2020-12-17 11:19:39 +0300

    добавлен файл find_xpath_form

---
+++ find_xpath_form.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import time
+
+link = " http://suninjuly.github.io/find_xpath_form"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element_by_tag_name("input")
+    input1.send_keys("Ekaterina")
+    input2 = browser.find_element_by_name("last_name")
+    input2.send_keys("Zhukova")
+    input3 = browser.find_element_by_class_name("form-control.city")
+    input3.send_keys("Novosibirsk")
+    input4 = browser.find_element_by_id("country")
+    input4.send_keys("Russia")
+    button = browser.find_element_by_xpath ('//button[@type="submit"]')
+    button.click()
+
+finally:
+    time.sleep(30)
+    browser.quit()
